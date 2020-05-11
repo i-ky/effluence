6a09ba994e9b48c4a662288cea3aea655cbb8b51
i-ky <gl.ivanovsky@gmail.com>
2020-05-11 21:26:10 +0000

    added header with declaration of size_t

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,8 @@
 #ifndef EFFLU_UTILS_H
 #define EFFLU_UTILS_H
 
+#include <stddef.h>
+
 #ifndef __GNUC__
 #	warning "Expect to get less warnings because your compiler does not support attributes"
 #	define __attribute__(x)
